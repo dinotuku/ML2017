12218082a369e0db87678cc4bd9ac3dc7dd9c322
dtk0528 <b03901039@ntu.edu.tw>
2017-03-22 10:58:17 +0800

    Update hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -7,7 +7,7 @@ train_data_name = sys.argv[1]
 test_data_name = sys.argv[2]
 output_name = sys.argv[3]
 
-# np.random.seed(0)
+np.random.seed(0)
 pd.options.mode.chained_assignment = None
 
 
@@ -19,7 +19,7 @@ def read_training_data(name):
   # replace NR by 0
   df.fillna(value=0, inplace=True)
   
-  # drop 測站名
+  # drop station name
   df.drop(1, axis=1, inplace=True)
 
   data = {}
@@ -49,21 +49,21 @@ def read_training_data(name):
   return data
 
 
-def process_training_data(data):
+def process_training_data(data, fl, p):
 
   train_x = []
   train_y = []
 
-  for i in range(1, 7):
-
+  for i in range(1, 13):
+    # get each month data
     X = data[str(i).zfill(2)].values
 
     for j in range(len(X[0]) - 9):
 
       tmp_x = []
 
-      for k in feat_list:
-
+      for k in fl:
+        # add 9 hours of data into each group of data
         tmp_x.append(X[k][j:j + 9])
 
       train_x.append(tmp_x)
@@ -75,7 +75,7 @@ def process_training_data(data):
   train_y = np.array(train_y)
 
   train_x, m, d = normalize(train_x)
-  train_x = powering(train_x, power)
+  train_x = powering(train_x, p)
 
   train_size = 2700
   indices = np.random.permutation(train_x.shape[0])
@@ -126,77 +126,75 @@ def cal_rmse(x, w, b, y):
   return np.sqrt(((y - b - (x*w).sum(axis=1).sum(axis=1)) ** 2).sum() / len(y))
 
 
-power = 2
+def linear_regression(x, y, vx, vy, m, d, fl, p):
 
-
-def linear_regression(x, y, vx, vy, m, d):
-
-  fNum = x.shape[1]
   lr = 0.04
   batch = x.shape[0] * 0.02
-  print_every = 10
+  print_every = 100
   save_every = 100
-  iteration = 2000
+  iteration = 1000
 
   w = np.random.rand(x.shape[1], x.shape[2])
   b = 0
 
   b_lr = 0.0
-  w_lr = np.zeros((fNum, x.shape[2]))
-  lamb = 0.001
+  w_lr = np.zeros((x.shape[1], x.shape[2]))
+  lamb = 0.01
+  momen = 0.35
 
-  b_history = np.array(b)
-  w_history = np.array(w)
+  delta_b = 0
+  delta_w = np.zeros((x.shape[1], x.shape[2]))
 
   print('Starting to train model')
 
   for i in range(iteration + 1):
 
     b_grad = 0.0
-    w_grad = np.zeros((fNum, x.shape[2]))
+    w_grad = np.zeros((x.shape[1], x.shape[2]))
 
     batchNum = 0
-
+    # mini-batch training
     for n in np.random.permutation(x.shape[0]):
 
       if batchNum < batch:
 
         batchNum += 1
-        b_grad = b_grad - 2.0*(y[n] - b - (w*x[n]).sum())
-        w_grad = w_grad - 2.0*(y[n] - b - (w*x[n]).sum())*x[n]
+        b_grad = b_grad - 2.0 * (y[n] - b - (w*x[n]).sum())
+        w_grad = w_grad - 2.0 * (y[n] - b - (w*x[n]).sum()) * x[n]
 
       else:
 
-        b_grad = b_grad - 2.0*(y[n] - b - (w*x[n]).sum())
-        w_grad = w_grad - 2.0*(y[n] - b - (w*x[n]).sum())*x[n]
-        # adagrad
+        b_grad = b_grad - 2.0 * (y[n] - b - (w*x[n]).sum())
+        w_grad = w_grad - 2.0 * (y[n] - b - (w*x[n]).sum()) * x[n]
+        
+        # adagrad and momentum
         b_lr += b_grad ** 2
         w_lr += w_grad ** 2
-        b = b - lr/np.sqrt(b_lr / (i+1)) * b_grad
-        # w = w - lr/np.sqrt(w_lr / (i+1)) * w_grad
-        w = w - lr/np.sqrt(w_lr / (i+1)) * (w_grad + 2.0 * lamb * w)
+        delta_b = momen * delta_b + (1 - momen) * lr / np.sqrt(b_lr / (i+batchNum+1)) * b_grad
+        delta_w = momen * delta_w + (1 - momen) * lr / np.sqrt(w_lr / (i+batchNum+1)) * (w_grad + 2.0 * lamb * w)
+        b = b - delta_b
+        w = w - delta_w
+
         b_grad = 0.0
-        w_grad = np.zeros((fNum, x.shape[2]))
+        w_grad = np.zeros((x.shape[1], x.shape[2]))
         batchNum = 0
-        b_history = np.append(b_history, b)
-        w_history = np.append(w_history, w)
-    
+    # see through every data
     for n in range(x.shape[0]):
 
-      b_grad = b_grad - 2.0*(y[n] - b - (w*x[n]).sum())
-      w_grad = w_grad - 2.0*(y[n] - b - (w*x[n]).sum())*x[n]
+      b_grad = b_grad - 2.0 * (y[n] - b - (w*x[n]).sum())
+      w_grad = w_grad - 2.0 * (y[n] - b - (w*x[n]).sum()) * x[n]
 
-    # adagrad
+    # adagrad and momentum
     b_lr += b_grad ** 2
     w_lr += w_grad ** 2
-    b = b - lr/np.sqrt(b_lr / (i+1)) * b_grad
-    # w = w - lr/np.sqrt(w_lr / (i+1)) * w_grad
-    w = w - lr/np.sqrt(w_lr / (i+1)) * (w_grad + 2.0 * lamb * w)
+    delta_b = momen * delta_b + (1 - momen) * lr / np.sqrt(b_lr / (i+1)) * b_grad
+    delta_w = momen * delta_w + (1 - momen) * lr / np.sqrt(w_lr / (i+1)) * (w_grad + 2.0 * lamb * w)
+    b = b - delta_b
+    w = w - delta_w
+
     b_grad = 0.0
-    w_grad = np.zeros((fNum, x.shape[2]))
+    w_grad = np.zeros((x.shape[1], x.shape[2]))
     batchNum = 0
-    b_history = np.append(b_history, b)
-    w_history = np.append(w_history, w)
     
     trmse = cal_rmse(x, w, b, y)
     vrmse = cal_rmse(vx, w, b, vy)
@@ -204,11 +202,11 @@ def linear_regression(x, y, vx, vy, m, d):
     if i % print_every == 0:
 
       print('Iteration: %d, TRMSE: %.4f, VRMSE: %.4f' % (i, trmse, vrmse))
-
+        
     if i % save_every == 0:
 
       print('Saving model')
-      model = [w, b, m, d]
+      model = [w, b, m, d, fl, p]
       np.save('./model/model_' + str(i), model)
 
   return w, b
@@ -249,7 +247,7 @@ def read_testing_data(name):
   return data
 
 
-def process_testing_data(data, m, d, fl):
+def process_testing_data(data, m, d, fl, p):
 
   test_x = []
 
@@ -271,7 +269,7 @@ def process_testing_data(data, m, d, fl):
 
     test_x[i] = (test_x[i] - m) / d
 
-  test_x = powering(test_x, power)
+  test_x = powering(test_x, p)
 
   return test_x
 
@@ -319,28 +317,22 @@ def main():
     bias = mod[1]
     mean = mod[2]
     devi = mod[3]
-    if (sys.argv[4] == 'best'):
-      feat_list = [2, 7, 9, 15, 16]
-    elif (sys.argv[4] == 'base'):
-      feat_list = [2, 6, 7, 9, 12]
-    test_x = process_testing_data(test_data, mean, devi, feat_list)
+    feat_list = mod[4]
+    power = mod[5]
+    test_x = process_testing_data(test_data, mean, devi, feat_list, power)
     use_saved_model(test_x, weight, bias)
 
     return
 
-  train_data = read_training_data(train_data_name)
-  # feat_list = [2, 7, 9, 15, 16]
-  # feat_list = [2, 6, 7, 9, 12]
-  feat_list = [9]
-  train_x, train_y, val_x, val_y, mean, devi = process_training_data(train_data)
-  weight, bias = linear_regression(train_x, train_y, val_x, val_y, mean, devi)
+  feat_list = [2, 6, 7, 9, 12]
+  power = 2
 
-  print('Saving final model')
-  model = [weight, bias, mean, devi]
-  np.save('model', model)
+  train_data = read_training_data(train_data_name)
+  train_x, train_y, val_x, val_y, mean, devi = process_training_data(train_data, feat_list, power)
+  weight, bias = linear_regression(train_x, train_y, val_x, val_y, mean, devi, feat_list, power)
 
   test_data = read_testing_data(test_data_name)
-  test_x = process_testing_data(test_data, mean, devi)
+  test_x = process_testing_data(test_data, mean, devi, feat_list, power)
   test_y = use_model(test_x, weight, bias)
 
   output_result(test_y, output_name)
